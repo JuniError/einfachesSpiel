08d79c59fa878bca5e8eda0a32307fd9a49ab6ee
Julian <julian2rellop@gmail.com>
2026-09-23 09:38:28 +0200

    Restlichen Layouts erstellt

--- src/jpoller/view/GrafikPanel.java
+++ src/jpoller/view/GrafikPanel.java
@@ -2,9 +2,12 @@ package jpoller.view;
 import javax.swing.*;
 import java.awt.*;
 
+import static javax.swing.BoxLayout.PAGE_AXIS;
+
 public class GrafikPanel extends JPanel{
     private JLabel rde = new JLabel("Tippe eine Zahl von 1 bis 9");
     private JLabel gsp = new JLabel("Gesamtpunkte: ");
+    private JTextField[] txtary = new JTextField[2];
 
     public GrafikPanel() {
         JPanel oben = new JPanel(new GridLayout(3, 2));
@@ -14,6 +17,17 @@ public class GrafikPanel extends JPanel{
         oben.add(gsp);
         oben.add(new JLabel("Deine Zahl:"));
         oben.add(new JLabel("Computer:"));
+
+        JPanel mitte = new JPanel(new GridLayout(1, 2));
+        for(int i = 0; i < 2; i++) {
+            txtary[i] = new JTextField();
+            txtary[i].setHorizontalAlignment(JTextField.CENTER);
+            mitte.add(txtary[i]);
+        }
+
+        JPanel unten = new JPanel();
+        unten.setLayout(new BoxLayout(unten, PAGE_AXIS));
+
     }
 
     public void setErgebnis(int z, int check) {
