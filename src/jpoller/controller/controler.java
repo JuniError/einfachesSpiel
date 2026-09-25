191e83a82423737ddbc4d283b8f5778126ffefc9
Julian <julian2rellop@gmail.com>
2026-09-25 10:58:02 +0200

    Erste Version GUI fertig

--- src/jpoller/controller/controler.java
+++ src/jpoller/controller/controler.java
@@ -22,18 +22,27 @@ public class controler implements ActionListener {
         String cmd = e.getActionCommand();
         if(cmd.equals("ne")) {
             for (int i = 0; i < 2; i++) {
-                this.frame.setTxt("", i);
+                this.frame.getGrafikPanel().setTxt("", i);
             }
-            this.frame.setRundenErgebnis("Tippe eine Zahl von 1 bis 9");
+            this.frame.getGrafikPanel().setRundenErgebnis("Tippe eine Zahl von 1 bis 9");
+            this.frame.getGrafikPanel().getTxtField(0).setEnabled(true);
         } else if(cmd.equals("txt")) {
-            this.eingabe = Integer.parseInt(this.frame.getTxt(0));
+            this.eingabe = Integer.parseInt(this.frame.getGrafikPanel().getTxt(0));
             this.gm.berechneRunde(this.eingabe);
-            this.frame.setGesamtErgebnis("" + gm.getGesamtPunkte());
-            if(Integer.parseInt(this.frame.getGesamtErgebnis()) <= 0) {
-                this.frame.setRundenErgebnis("Verloren!");
+            this.frame.getGrafikPanel().setGesamtErgebnis("" + this.gm.getGesamtPunkte());
+            this.frame.getGrafikPanel().setRundenErgebnis("" + this.gm.getRundenErgebnis());
+            this.frame.getGrafikPanel().setGesamtErgebnis("" + this.gm.getGesamtPunkte());
+            this.frame.getGrafikPanel().setTxt("" + this.gm.getComputerZahl(), 1);
+            if(this.gm.hatVerloren()) {
+                this.frame.getGrafikPanel().setRundenErgebnis("Verloren!");
+                this.frame.getGrafikPanel().getTxtField(0).setEnabled(false);
+                this.frame.getGrafikPanel().getButton().setEnabled(false);
+            } else if(this.gm.hatGewonnen()) {
+                this.frame.getGrafikPanel().setRundenErgebnis("Gewonnen!");
+                this.frame.getGrafikPanel().getTxtField(0).setEnabled(false);
+                this.frame.getGrafikPanel().getButton().setEnabled(false);
             }
-            this.frame.setRundenErgebnis("" + gm.getRundenErgebnis());
-
+            this.frame.getGrafikPanel().getTxtField(0).setEnabled(false);
         }
     }
 
--- src/jpoller/view/GrafikFrame.java
+++ src/jpoller/view/GrafikFrame.java
@@ -24,27 +24,7 @@ public class GrafikFrame extends JFrame {
         jt.setActionCommand("txt");
     }
 
-    public void setRundenErgebnis(String s) {
-        this.gp.setRundenErgebnis(s);
-    }
-
-    public String getRundenErgebnis() {
-        return this.gp.getRundenErgebnis();
-    }
-
-    public String getGesamtErgebnis() {
-        return this.gp.getGesamtErgebnis();
-    }
-
-    public void setGesamtErgebnis(String s) {
-        this.gp.setGesamtErgebnis(s);
-    }
-
-    public String getTxt(int i) {
-        return this.gp.getTxt(i);
-    }
-
-    public void setTxt(String s, int i) {
-        this.gp.setTxt(s, i);
+    public GrafikPanel getGrafikPanel() {
+        return this.gp;
     }
 }
