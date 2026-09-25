675ddf4a3d73771f81fa0edae8cfe3d680a079db
Julian <julian2rellop@gmail.com>
2026-09-25 11:19:47 +0200

    Coloring V2.0

--- src/jpoller/controller/controler.java
+++ src/jpoller/controller/controler.java
@@ -3,6 +3,7 @@ package jpoller.controller;
 import jpoller.view.GrafikFrame;
 import jpoller.model.GewinnModel;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class controler implements ActionListener {
@@ -33,6 +34,17 @@ public class controler implements ActionListener {
             this.frame.getGrafikPanel().setRundenErgebnis("" + this.gm.getRundenErgebnis());
             this.frame.getGrafikPanel().setGesamtErgebnis("" + this.gm.getGesamtPunkte());
             this.frame.getGrafikPanel().setTxt("" + this.gm.getComputerZahl(), 1);
+            if(this.gm.getRundenErgebnis() < 0) {
+                for (int i = 0; i < 2; i++) {
+                    this.frame.getGrafikPanel().getJLabel(i).setOpaque(true);
+                    this.frame.getGrafikPanel().getJLabel(i).setBackground(Color.RED);
+                }
+            } else {
+                for (int i = 0; i < 2; i++) {
+                    this.frame.getGrafikPanel().getJLabel(i).setOpaque(true);
+                    this.frame.getGrafikPanel().getJLabel(i).setBackground(Color.GREEN);
+                }
+            }
             if(this.gm.hatVerloren()) {
                 this.frame.getGrafikPanel().setRundenErgebnis("Verloren!");
                 this.frame.getGrafikPanel().getTxtField(0).setEnabled(false);
